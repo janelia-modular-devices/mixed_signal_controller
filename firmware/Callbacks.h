c35a3afc73b4c7f1e668ddc128edac767e5d72c3
Peter Polidoro <peter@polidoro.io>
2021-09-17 10:12:10 -0400

    Update email address

--- firmware/Callbacks.h
+++ firmware/Callbacks.h
@@ -3,7 +3,7 @@
 //
 //
 // Authors:
-// Peter Polidoro peterpolidoro@gmail.com
+// Peter Polidoro peter@polidoro.io
 // ----------------------------------------------------------------------------
 #ifndef CALLBACKS_H
 #define CALLBACKS_H
